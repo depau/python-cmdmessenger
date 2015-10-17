942f4c406c2d0cd9853751e4f8c01bbf35957a75
Adrien Emery <adrien.emery@gmail.com>
2015-10-16 21:41:53 -0700

    Remove seperate serial thread

--- sendandrecieve.py
+++ sendandrecieve.py
@@ -11,8 +11,6 @@ from cmdmessenger import CmdMessenger
 from serial.tools import list_ports
 from serialmonitor import SerialMonitor
 
-ON = 1
-OFF = 0
 
 def list_usb_ports():
     """
@@ -32,13 +30,15 @@ def on_status(received_command, *args, **kwargs):
     Callback function that prints the Arduino status to the console
     """
     print "Arduino status: ", args[0][0]
-    # status = args[0][0]
 
 class Commands(object):
     set_led = 0
     status = 1
 
 if __name__ == '__main__':
+    ON = 1
+    OFF = 0
+    led_state = OFF
     baud = 115200  # make sure this matches the baudrate on the Arduino
 
     try:
@@ -56,20 +56,23 @@ if __name__ == '__main__':
         messenger.attach(func=on_unknown_command)
         messenger.attach(func=on_status, msgid=Commands.status)
 
-        # start the serial monitor thread
-        # serial_monitor = SerialMonitor(serial_port, messenger)
-        # serial_monitor.start()
-
     try:
         print 'Press Ctrl+C to exit...'
-        # run a loop to turn on and off the LED once a second
+        t0 = time.clock()
+
         while True:
-            messenger.send_cmd(Commands.set_led, ON)
-            # wait for a response:
+            # Update the led state once every second
+            if time.clock() - t0 > 1:
+                t0 = time.clock()
+                if led_state == ON:
+                    messenger.send_cmd(Commands.set_led, OFF)
+                    led_state = OFF
+                else:
+                    messenger.send_cmd(Commands.set_led, ON)
+                    led_state = ON
+
+            # Check to see if any data has been recieved
             messenger.feed_in_data()
-            messenger.send_cmd(Commands.set_led, OFF)
-            messenger.feed_in_data()
+
     except KeyboardInterrupt:
-        # kill the serial monitor thread
-        # serial_monitor.stop()
         print 'Exiting...'
