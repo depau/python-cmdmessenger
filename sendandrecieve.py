56c93be53356efca5abcce9c33d647f5e48f3a12
Adrien Emery <adrien.emery@gmail.com>
2015-10-16 22:29:00 -0700

    Move everything into a class

--- sendandrecieve.py
+++ sendandrecieve.py
@@ -11,68 +11,82 @@ from cmdmessenger import CmdMessenger
 from serial.tools import list_ports
 from serialmonitor import SerialMonitor
 
+ON = 1
+OFF = 0
 
-def list_usb_ports():
-    """
-    Use the grep generator to get a list of all USB ports.
-    """
-    ports =  [port for port in list_ports.grep('usb')]
-    return ports
-
-def on_unknown_command(self, received_command, *args, **kwargs):
-    """
-    Executes when an unknown command has been received.
-    """
-    print "Command without attached callback received"
-
-def on_status(received_command, *args, **kwargs):
-    """
-    Callback function that prints the Arduino status to the console
-    """
-    print "Arduino status: ", args[0][0]
 
 class Commands(object):
     set_led = 0
     status = 1
 
-if __name__ == '__main__':
-    ON = 1
-    OFF = 0
-    led_state = OFF
-    baud = 115200  # make sure this matches the baudrate on the Arduino
 
-    try:
-        # gets the first available USB port
-        port_name = list_usb_ports()[0][0]
-        serial_port = serial.Serial(port_name, baud, timeout=0)
-    except (serial.SerialException, IndexError):
-        print 'Could not open serial port.'
-        sys.exit(1)
-    else:
-        print 'Serial port sucessfully opened.'
-        messenger = CmdMessenger(serial_port)
+class SendAndRecieve(object):
 
-        # attach callbacks
-        messenger.attach(func=on_unknown_command)
-        messenger.attach(func=on_status, msgid=Commands.status)
+    def __init__(self):
+        # make sure this baudrate matches the baudrate on the Arduino
+        self.running = False
+        self.led_state = OFF
+        self.baud = 115200
 
-    try:
-        print 'Press Ctrl+C to exit...'
+        try:
+            # gets the first available USB port
+            self.port_name = self.list_usb_ports()[0][0]
+            self.serial_port = serial.Serial(self.port_name, self.baud, timeout=0)
+        except (serial.SerialException, IndexError):
+            print 'Could not open serial port.'
+            self.messenger = None
+        else:
+            print 'Serial port sucessfully opened.'
+            self.messenger = CmdMessenger(self.serial_port)
+            # attach callbacks
+            self.messenger.attach(func=self.on_unknown_command)
+            self.messenger.attach(func=self.on_status, msgid=Commands.status)
+
+    def list_usb_ports(self):
+        """
+        Use the grep generator to get a list of all USB ports.
+        """
+        ports =  [port for port in list_ports.grep('usb')]
+        return ports
+
+    def on_unknown_command(self, received_command, *args, **kwargs):
+        """
+        Executes when an unknown command has been received.
+        """
+        print "Command without attached callback received"
+
+    def on_status(self, received_command, *args, **kwargs):
+        """
+        Callback function that prints the Arduino status to the console
+        """
+        print "Status: ",  args[0][0]
+
+    def stop(self):
+        self.running = False
+
+    def run(self):
+        self.running = True
         t0 = time.clock()
-
-        while True:
+        while self.running:
             # Update the led state once every second
             if time.clock() - t0 > 1:
                 t0 = time.clock()
-                if led_state == ON:
-                    messenger.send_cmd(Commands.set_led, OFF)
-                    led_state = OFF
+                if self.led_state == ON:
+                    self.messenger.send_cmd(Commands.set_led, OFF)
+                    self.led_state = OFF
                 else:
-                    messenger.send_cmd(Commands.set_led, ON)
-                    led_state = ON
+                    self.messenger.send_cmd(Commands.set_led, ON)
+                    self.led_state = ON
 
             # Check to see if any data has been recieved
-            messenger.feed_in_data()
+            self.messenger.feed_in_data()
 
+if __name__ == '__main__':
+    send_and_recieve = SendAndRecieve()
+
+    try:
+        print 'Press Ctrl+C to exit...'
+        send_and_recieve.run()
     except KeyboardInterrupt:
+        send_and_recieve.stop()
         print 'Exiting...'
